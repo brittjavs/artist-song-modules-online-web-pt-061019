8bdd39c5070263dee7c7096eff5908ec8c4eb8ba
Brittany Javalera <brittjavalera@gmail.com>
2019-07-16 18:53:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,6 @@ require 'pry'
 
 class Song
   include Paramable
-  include Findable
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   attr_accessor :name
@@ -14,9 +13,9 @@ class Song
     super
   end
 
-  # def self.find_by_name(name)
-  #   @@songs.detect{|a| a.name == name}
-  # end
+  def self.find_by_name(name)
+    @@songs.detect{|a| a.name == name}
+  end
 
   def self.all
     @@songs
